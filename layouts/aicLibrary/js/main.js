b217e75c43be3e5b48cfaee917a8f12565904227
Astelth <astelth17@gmail.com>
2023-08-29 16:53:37 +0300

    +

--- layouts/aicLibrary/js/main.js
+++ layouts/aicLibrary/js/main.js
@@ -4,3 +4,29 @@ function burgerClick() {
     document.querySelector('.mobile').classList.toggle('mobile_active');
     document.querySelector('.top').classList.toggle('top_active');
 }
+
+
+// Инициализация слайдера изображений
+const sliderImages = new Swiper('.slider__images .swiper-container', { // ищем слайдер превью по селектору
+	// задаем параметры
+	direction: 'vertical', // вертикальная прокрутка
+	slidesPerView: 1, // показывать по 1 изображению
+	spaceBetween: 32, // расстояние между слайдами
+	mousewheel: true, // можно прокручивать изображения колёсиком мыши
+	navigation: { // задаем кнопки навигации
+		nextEl: '.slider__next', // кнопка Next
+		prevEl: '.slider__prev' // кнопка Prev
+	},
+	grabCursor: true, // менять иконку курсора
+	thumbs: { // указываем на превью слайдер
+		swiper: sliderThumbs // указываем имя превью слайдера
+	},
+	breakpoints: { // условия для разных размеров окна браузера
+		0: { // при 0px и выше
+			direction: 'horizontal', // горизонтальная прокрутка
+		},
+		768: { // при 768px и выше
+			direction: 'vertical', // вертикальная прокрутка
+		}
+	}
+});
