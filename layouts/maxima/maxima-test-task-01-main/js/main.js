bb15402c67e40f2b121e37431404211f1ac05e10
Astelth <astelth17@gmail.com>
2023-09-20 17:12:15 +0300

    maxima lessons

---
+++ layouts/maxima/maxima-test-task-01-main/js/main.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 300,
+    navigation: {
+      nextEl: ".swiper__button-next",
+      prevEl: ".swiper__button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+ 
