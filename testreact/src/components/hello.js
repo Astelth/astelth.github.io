ce1d348d8d6211056947acf0b7562d66c04f186a
unknown <astelth17@gmail.com>
2024-03-23 21:13:22 +0300

    '+'

--- testreact/src/components/hello.js
+++ testreact/src/components/hello.js
@@ -9,7 +9,7 @@ class Hello extends React.Component {
     render() {
         return (<div className='hello'>
             <div onClick={helloClick} onMouseOver={mouseOver}>{this.props.title}</div>
-            <h3>Привет участникам соревнований</h3>
+            <h3 className='title'>Привет участникам соревнований</h3>
             <div className="ass">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             </div>
