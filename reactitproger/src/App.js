27255f58bbc9cca37ddc428cab07182490d97d23
Andrey Stepanenko <astelth17@gmail.com>
2024-06-16 11:27:17 +0300

    fix?

--- reactitproger/src/App.js
+++ reactitproger/src/App.js
@@ -10,19 +10,34 @@ import Logo from "./img/logo.png"
 //     );
 
 class App extends React.Component {
-    helpText = "Help text!!!";
+    constructor(props) {
+        super(props)
+        this.state = {
+            helpText: 'Help text!!!',
+            userData: ''
+        }
+
+        this.inputClick = this.inputClick.bind(this)
+    }
+
     render() {
         return (<div classname="name">
             <Header title='Шапка сайта' />
             <Header title='Шапка сайта!!!' />
-            <h1>{this.helpText}</h1>
-            <input placeholder={this.helpText}
+            <h1>{this.state.helpText}</h1>
+            <h2>{this.state.userData}</h2>
+            <input placeholder={this.state.helpText}
+                onChange={event => this.setState({ userData: event.target.value })}
                 onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-            <p>{this.helpText === "Help text!!!" ? "Yes" : "No"}</p>
+            <p>{this.state.helpText === "Help text!!!" ? "Yes" : "No"}</p>
             <Image image={Logo} />
         </div>)
     }
-    inputClick() { console.log('Clicked') };
+    inputClick() {
+        console.log('Clicked');
+        this.setState({ helpText: 'Changed' })
+        //this.state.helpText === this.state.helpTextDefault ? this.state.helpText = 'Changed' : this.state.helpText = this.state.helpTextDefault;
+    };
     mouseOver() { console.log('Mouse over') };
 }
 
