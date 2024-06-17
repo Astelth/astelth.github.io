5d068d77b4472f9da1b48c296aa0d269e3e482d6
Astelth <astelth17@gmail.com>
2024-06-17 12:56:03 +0300

    part6_3_52

--- reactitproger/src/components/Header.js
+++ reactitproger/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from "./Button";
 
 class Header extends React.Component {
     render() {
         return (
+            <div>
             <header className="header">{ this.props.title }</header>
+            <Button text='привет' />
+            </div>
         )
     }
 }
--- reactitproger/src/components/Image.js
+++ reactitproger/src/components/Image.js
@@ -3,7 +3,7 @@ import React from "react";
 class Image extends React.Component {
     render() {
         return (
-            <img src={this.props.image} className="logo" />
+            <img src={this.props.image} className="logo" alt='изображение' />
         )
     }
 }
