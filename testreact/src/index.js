685506a6c091bc6d9d73654e2210ec709879e371
Andrey Stepanenko <astelth17@gmail.com>
2024-06-22 23:35:04 +0300

    delete reactitproger

--- testreact/src/index.js
+++ testreact/src/index.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-import Hello from './components/hello';
+import React from "react";
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App';
+import './css/main.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <div className='grid'>
-      <Hello title="Здравствуй, жопа, Новый год! Приходи на ёлку!" className='hello' />
-      <App className='app' />
-    </div>
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = ReactDOMClient.createRoot(document.getElementById("app"));
+
+app.render(<App />);
